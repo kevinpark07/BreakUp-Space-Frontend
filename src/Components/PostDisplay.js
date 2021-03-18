cb3cbed6886377ed0dd4cb8a5522463a2abb77f6
kevinpark07 <kevin.park07@yahoo.com>
2021-03-18 14:23:06 -0400

    Community Page Modal CSS

--- src/Components/PostDisplay.js
+++ src/Components/PostDisplay.js
@@ -52,7 +52,8 @@ const PostDisplay = props => {
         <Container>
             <Header>
                 <h1>Community Board</h1>
-                {props.user ? <Button onClick={handleOpen}>Write Post</Button> : <h4>Must be logged in to Post or Comment. <br></br><a href="/login">Log-in</a>?</h4>}
+                <Button onClick={handleOpen}>Write Post</Button>
+                {/* {props.user ? <Button onClick={handleOpen}>Write Post</Button> : <h4>Must be logged in to Post or Comment. <br></br><a href="/login">Log-in</a>?</h4>} */}
             </Header>
             <PostContainer>
                 {renderPosts()}
@@ -61,14 +62,18 @@ const PostDisplay = props => {
                 open={open}
                 onClose={handleClose}
             >
-                <div>
-                    <form onSubmit={handleSubmit}>
-                        <input type='text' name='title' placeholder='title goes here!' onChange={changeHandle} />
-                        <input type='textarea' name='content' placeholder='text goes here!' onChange={changeHandle} />
-                        <input type='file' name='image' onChange={changeHandle} />
-                        <button>Submit</button>
-                    </form>
-                </div>
+                <FormContainer>
+                    <Form onSubmit={handleSubmit}>
+                        <h1>Create a Post</h1>
+                        <label>Title</label>
+                        <Input type='text' name='title' placeholder='Title' onChange={changeHandle} />
+                        <label>Content</label>
+                        <TextArea name='content' placeholder='Text (optional)' onChange={changeHandle} />
+                        <label>Choose File</label>
+                        <Input type='file' name='image' onChange={changeHandle} />
+                        <FormButton>Submit</FormButton>
+                    </Form>
+                </FormContainer>
             </Modal>
         </Container>
     )
@@ -88,6 +93,56 @@ const mdp = dispatch => {
 }
 
 export default connect(msp, mdp)(PostDisplay);
+
+const FormContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 500px;
+    height: auto;
+    background: #EAEAEA;
+`
+
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border: solid blue;
+    width: 500px;
+`
+
+const Input = styled.input`
+    margin-bottom: 5%;
+    border: solid green;
+`
+
+const TextArea = styled.textarea`
+    margin-bottom: 5%;
+    width: 
+    border: solid orange;
+`
+
+const FormButton = styled.button`
+    white-space: nowrap;
+    width: 110px;
+    border-radius: 12px;
+    border: #bfa0e2;
+    font-weight: 600;
+    color: white;
+    background-color: #bfa0e2;
+    font-size: 14px;
+    text-align: center;
+    padding: 12px 0px;
+    &:hover {
+        cursor: pointer;
+    }
+`
 
 const Container = styled.div`
     display: flex;
